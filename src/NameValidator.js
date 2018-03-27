d043dc5de4b25c8701b6bc13f30453f9cb43739a
ololokiras <bellendirr@yandex.ru>
2018-03-27 21:46:33 +0800

    move validation to separate file

---
+++ src/NameValidator.js
@@ -0,0 +1,62 @@
+function isSymbolIsLetter(symbol) {
+  if (!(symbol >= 'a' && symbol <= 'z')) return false;
+  return true;
+}
+
+function isSymbolIsDigit(symbol) {
+  if (!(symbol >= '0' && symbol <= '9')) return false;
+  return true;
+}
+
+function isValidSequence(sequence) {
+  for (let i = 0; i < sequence.length; i += 1) {
+    if (!(isSymbolIsLetter(sequence.charAt(i)) ||
+        isSymbolIsDigit(sequence.charAt(i)) ||
+        sequence.charAt(i) === '-')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isNameValid(name) {
+  const labels = name.split('.');
+  for (let i = 0; i < labels.length; i += 1) {
+    if (!isSymbolIsLetter(labels[i].charAt(0))) {
+      return false;
+    }
+
+    if (!(isSymbolIsLetter(labels[i].charAt(labels[i].length - 1)) ||
+      isSymbolIsDigit(labels[i].length - 1))) {
+      return false;
+    }
+
+    if (!isValidSequence(labels[i])) return false;
+  }
+  return true;
+}
+
+function isCheapName(name) {
+  for (let i = 0; i < name.length; i += 1) {
+    const c = name.charAt(i);
+    if (c >= '0' && c <= '9') return true;
+    if (c === '.' || c === '-' || c === '/') return true;
+    switch (c) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+      case 'y':
+        return true;
+      default:
+        break;
+    }
+  }
+  return false;
+}
+
+module.exports = {
+  isNameValid,
+  isCheapName
+};
--- src/worker.js
+++ src/worker.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { userRegistration } = require('./AccountCreator');
 const { key } = require('bitsharesjs');
 const MoneySender = require('./MoneySender');
+const { isNameValid, isCheapName } = require('./NameValidator');
 
 function getPrivateKey(brainkey) {
   const normalizedBrainkey = key.normalize_brainKey(brainkey);
@@ -12,64 +13,6 @@ function getPrivateKey(brainkey) {
 }
 
 let ipTime = {};
-
-function isSymbolIsLetter(symbol) {
-  if (!(symbol >= 'a' && symbol <= 'z')) return false;
-  return true;
-}
-
-function isSymbolIsDigit(symbol) {
-  if (!(symbol >= '0' && symbol <= '9')) return false;
-  return true;
-}
-
-function isValidSequence(sequence) {
-  for (let i = 0; i < sequence.length; i += 1) {
-    if (!(isSymbolIsLetter(sequence.charAt(i)) ||
-        isSymbolIsDigit(sequence.charAt(i)) ||
-        sequence.charAt(i) === '-')) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function isNameValid(name) {
-  const labels = name.split('.');
-  for (let i = 0; i < labels.length; i += 1) {
-    if (!isSymbolIsLetter(labels[i].charAt(0))) {
-      return false;
-    }
-
-    if (!(isSymbolIsLetter(labels[i].charAt(labels[i].length - 1)) ||
-      isSymbolIsDigit(labels[i].length - 1))) {
-      return false;
-    }
-
-    if (!isValidSequence(labels[i])) return false;
-  }
-  return true;
-}
-
-function isCheapName(name) {
-  for (let i = 0; i < name.length; i += 1) {
-    const c = name.charAt(i);
-    if (c >= '0' && c <= '9') return true;
-    if (c === '.' || c === '-' || c === '/') return true;
-    switch (c) {
-      case 'a':
-      case 'e':
-      case 'i':
-      case 'o':
-      case 'u':
-      case 'y':
-        return true;
-      default:
-        break;
-    }
-  }
-  return false;
-}
 
 function clearAddressesTimeout() {
   const now = Date.now();
